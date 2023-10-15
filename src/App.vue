2cfaa14c749e9b7bc0d98ff7ceb80ad56cdf5631
Author Name <sh190887@proton.me>
2023-10-15 16:10:46 +0900

    Add score logic + random / not random logic + css dynamic behavior for question and left buttons

--- src/App.vue
+++ src/App.vue
@@ -3,27 +3,67 @@ import { ref } from "vue"
 import dataset from './assets/words.json'
 
 
-const questionText = ref("")
+const questionText = ref("level 1 : Greetings and Introduction")
 const rightButtonText = ref("click to start")
+const leftButtonText = ref("")
+const orderText = ref("RANDOM")
+const leftButtonIsActive = ref(false)
+const questionButtonIsActive = ref(false)
 const word = ref("")
 const translation = ref("")
 const units = ref(dataset["datasets"])
 const currentLevel = ref(0)
 const currentUnit = ref(1)
+const score = ref(0)
+const nbQuestion = ref(10)
+const gameIsOn = ref(false)
 let wordId = 0
+let random = true
+let nbTries = 0
+let unitLength = dataset["datasets"][currentLevel.value]["unit" + currentUnit.value]["vocabulary"].length
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
 function showTranslation() {
-  questionText.value === word.value ? questionText.value = translation.value : questionText.value = word.value
+  if(gameIsOn.value) {
+    questionText.value === word.value ? questionText.value = translation.value : questionText.value = word.value
+  }
 }
 
-function nextWord(level, unit, random) {
-  random ? wordId = getRandomInt(dataset["datasets"][level]["unit" + unit]["vocabulary"].length) : 
+function resetScore(){
+  random ? nbQuestion.value = 10 : nbQuestion.value = unitLength
+  nbTries = 0
+  score.value = 0
+}
+
+function setOrder() {
+  orderText.value === "RANDOM" ? orderText.value = "NOT RANDOM" : orderText.value = "RANDOM"
+  random = !random
+  resetScore()
+}
+
+function checkScore(){
+  if(nbTries===nbQuestion.value){
+    alert("your score : " + score.value + " / " + nbQuestion.value)
+    score.value = 0
+    nbTries = 0
+  }
+}
+
+function nextWord(level, unit) {
+  
+  random ? wordId = getRandomInt(dataset["datasets"][level]["unit" + unit]["vocabulary"].length) : null
 
   rightButtonText.value = "Got it!"
+  leftButtonText.value = "Study again"
+
+  leftButtonIsActive.value = true
+  questionButtonIsActive.value = true
+  gameIsOn.value = true
+
+
   word.value = units.value[level]["unit" + unit]["vocabulary"][wordId]["word"]
   translation.value = units.value[level]["unit" + unit]["vocabulary"][wordId]["translation"]
   questionText.value = word.value
@@ -31,13 +71,39 @@ function nextWord(level, unit, random) {
   !random && wordId === dataset["datasets"][level]["unit" + unit]["vocabulary"].length - 1 ? wordId = 0 : wordId++
 }
 
+function gotIt(level, unit){
+  if(gameIsOn.value) {
+    score.value++
+    nbTries++
+    checkScore()
+  }
+  nextWord(level, unit)
+}
+
+function studyAgain(level, unit) {
+  if(gameIsOn.value) {
+    nbTries++
+    checkScore()
+    nextWord(level, unit)
+  }
+}
+
 function changeUnit(level, unit, text) {
   let textLevel = level+1
   questionText.value = "level " + textLevel + " : " + text
   rightButtonText.value = "click to start"
+  leftButtonText.value = ""
+
+  leftButtonIsActive.value = false
+  questionButtonIsActive.value = false
+  gameIsOn.value = false
+
   currentLevel.value = level
   currentUnit.value = unit
+
+  unitLength = dataset["datasets"][currentLevel.value]["unit" + currentUnit.value]["vocabulary"].length
   wordId = 0
+  resetScore()
 }
 </script>
 
@@ -56,12 +122,14 @@ function changeUnit(level, unit, text) {
             <a v-for="unit in units[1]" :key="unit.id" @click="changeUnit(1, unit.id, unit.name)">{{ unit.id + " : " + unit.name }}</a>
           </div>
         </li>
+        <li class="dropdown"><a @click="setOrder()" class="dropbtn">{{ orderText }}</a></li>
+        <li class="score"><span>SCORE : {{ score + "/" + nbQuestion}}</span></li>
       </ul>
     </header>
     <main>
-      <div @click="showTranslation()" class="center unselectable" id="top"><p>{{ questionText }}</p></div>
-      <div class="bottom center unselectable" id="left">Study again</div>
-      <div @click="nextWord(currentLevel, currentUnit, false)" class="bottom center unselectable" id="right">{{ rightButtonText }}</div>
+      <div @click="showTranslation()" class="center unselectable structure" :class="{ top: questionButtonIsActive }"><p>{{ questionText }}</p></div>
+      <div @click="studyAgain(currentLevel, currentUnit)" class="bottom center unselectable" :class="{ left: leftButtonIsActive }">{{ leftButtonText }}</div>
+      <div @click="gotIt(currentLevel, currentUnit)" class="bottom center unselectable right">{{ rightButtonText }}</div>
     </main>
   </div>
 </template>
@@ -90,7 +158,7 @@ ul {
 }
 
 main {
-  height: 92vh;
+  height: 91vh;
   font-size: 1.5em;
   display: flex;
   flex-direction: row;
@@ -103,7 +171,7 @@ main {
   justify-content: center;
 }
 
-#top {
+.structure {
   width: 100%;
   height: 75%;
 }
@@ -113,16 +181,16 @@ main {
   height: 25%;
 }
 
-#top:hover {
+.top:hover {
   cursor: help;
   color: grey;
 }
 
-#left:hover {
+.left:hover {
   background-color: rgba(255, 0, 0, 0.2);
 }
 
-#right:hover {
+.right:hover {
   background-color: rgba(0, 128, 0, 0.2);
 }
 
